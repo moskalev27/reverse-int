bd58d3ef52d101fce8cae70f36a5909bfb0848c7
Evgenii Moskalev(@moskalev27) <moskalev27@gmail.com>
2021-08-17 21:08:13 +0300

    feat: solve reverse digits task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n)
+    // console.log(n);
+    let str = n.toString();
+    return str.split("").reverse().join("");
 }
+// console.log(reverse(234));
